66d50e043bf87b66329c36099734ba27b890fdb9
Michal Podolec/Systems and Processes Innovation (SWQA) /SRPOL/Engineer/Samsung Electronics <m.podolec@samsung.com>
2025-12-15 16:12:54 +0100

    clipboard addition

--- cest-la-vue/src/views/HomePage.vue
+++ cest-la-vue/src/views/HomePage.vue
@@ -1,10 +1,22 @@
 <script setup>
 import { userList } from "../composables/useUserStore";
-import { computed } from "vue";
+import { computed, ref } from "vue";
+import { useUserStore } from "../stores/UserStore";
+const userStore = useUserStore();
 
-const shortUserList = computed(() => {
-  return userList.value.splice(0, 5);
-});
+import { useClipboardItems } from "@vueuse/core";
+
+function createClipboardItems(text) {
+  const mime = 'text/plain'
+  const blob = new Blob([text], { type: mime })
+  return new ClipboardItem({
+    [mime]: blob,
+  })
+}
+
+const { content, copy, copied, isSupported } = useClipboardItems();
+
+const clip = ref("4253");
 </script>
 
 <template>
@@ -15,12 +27,31 @@ const shortUserList = computed(() => {
       Whatever your mind desires!
     </p>
     <hr />
+    <h3>copy to clipboard:</h3>
+    <div v-if="isSupported">
+      <button @click="() => copy([createClipboardItems(clip)])">
+        <!-- by default, `copied` will be reset in 1.5s -->
+        <span v-if="!copied">Copy</span>
+        <span v-else>Copied!</span>
+      </button>
+      <p>
+        Current copied: <code>{{ content || "none" }}</code>
+      </p>
+    </div>
+    <p v-else>Your browser does not support Clipboard API</p>
+    <input v-model="clip" />
+    <!-- <button :disabled="!content" @click="console.log(content)">Copy!</button> -->
+    <hr />
     <ul>
-      <li v-for="user in shortUserList">
+      <li v-for="user in userStore.shortUserList">
         {{ user.name }}
       </li>
     </ul>
   </main>
 </template>
 
-<style></style>
+<style>
+button:disabled {
+  background-color: ghostwhite;
+}
+</style>
